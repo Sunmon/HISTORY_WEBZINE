06026285a7ee9057a1f6504a7d0718fe590e0224
jiyoungtt <jiyoungtt@naver.com>
2019-06-07 00:45:55 +0900

    html,css,js

---
+++ Webzine.js
@@ -0,0 +1,79 @@
+
+
+$(document).ready(function(){
+
+
+	$("#img1").hover(function(){
+		$("#img1").attr("src","timebarhover1.jpg");
+	},function(){
+		$("#img1").attr("src","timebar1.jpg");
+	})
+
+	$("#img2").hover(function(){
+		$("#img2").attr("src","timebarhover2.jpg");
+	},function(){
+		$("#img2").attr("src","timebar2.jpg");
+	})
+
+
+	$("#img3").hover(function(){
+		$("#img3").attr("src","timebarhover2.jpg");
+	},function(){
+		$("#img3").attr("src","timebar2.jpg");
+	})
+	$("#img4").hover(function(){
+		$("#img4").attr("src","timebarhover2.jpg");
+	},function(){
+		$("#img4").attr("src","timebar2.jpg");
+	})
+	$("#img5").hover(function(){
+		$("#img5").attr("src","timebarhover2.jpg");
+	},function(){
+		$("#img5").attr("src","timebar2.jpg");
+	})
+	$("#img6").hover(function(){
+		$("#img6").attr("src","timebarhover2.jpg");
+	},function(){
+		$("#img6").attr("src","timebar2.jpg");
+	})
+	$("#img7").hover(function(){
+		$("#img7").attr("src","timebarhover2.jpg");
+	},function(){
+		$("#img7").attr("src","timebar2.jpg");
+	})
+
+	$("#img8").hover(function(){
+		$("#img8").attr("src","timebarhover3.jpg");
+	},function(){
+		$("#img8").attr("src","timebar3.jpg");
+	})
+
+	$("#img1").click(function(){
+		$("#new1").css("display","block");
+	})
+	$("#img2").click(function(){
+		$("#new1").css("display","block");
+	})
+	$("#img3").click(function(){
+		$("#new1").css("display","block");
+	})
+	$("#img4").click(function(){
+		$("#new1").css("display","block");
+	})
+	$("#img5").click(function(){
+		$("#new1").css("display","block");
+	})
+	$("#img6").click(function(){
+		$("#new1").css("display","block");
+	})
+	$("#img7").click(function(){
+		$("#new1").css("display","block");
+	})
+	$("#img8").click(function(){
+		$("#new1").css("display","block");
+	})
+
+
+
+
+});//end of ready
