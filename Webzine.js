c707e5a98a4aebac5e652bc6bb7505ca97a45b7b
jiyoungtt <jiyoungtt@naver.com>
2019-06-10 00:42:41 +0900

    simplify the code

--- Webzine.js
+++ Webzine.js
@@ -4,49 +4,25 @@ $(document).ready(function(){
 
 
 	$("#img1").hover(function(){
-		$("#img1").attr("src","source/timebarhover1.jpg");
+	$("#img1").attr("src","source/timebarhover1.jpg");
 	},function(){
 		$("#img1").attr("src","source/timebar1.jpg");
 	})
 
-	$("#img2").hover(function(){
-		$("#img2").attr("src","source/timebarhover2.jpg");
+    
+	$(".timebar").hover(function(){
+	$(this).attr("src","source/timebarhover2.jpg");
 	},function(){
-		$("#img2").attr("src","source/timebar2.jpg");
+		$(this).attr("src","source/timebar2.jpg");
 	})
 
-
-	$("#img3").hover(function(){
-		$("#img3").attr("src","source/timebarhover2.jpg");
-	},function(){
-		$("#img3").attr("src","source/timebar2.jpg");
-	})
-	$("#img4").hover(function(){
-		$("#img4").attr("src","source/timebarhover2.jpg");
-	},function(){
-		$("#img4").attr("src","source/timebar2.jpg");
-	})
-	$("#img5").hover(function(){
-		$("#img5").attr("src","source/timebarhover2.jpg");
-	},function(){
-		$("#img5").attr("src","source/timebar2.jpg");
-	})
-	$("#img6").hover(function(){
-		$("#img6").attr("src","source/timebarhover2.jpg");
-	},function(){
-		$("#img6").attr("src","source/timebar2.jpg");
-	})
-	$("#img7").hover(function(){
-		$("#img7").attr("src","source/timebarhover2.jpg");
-	},function(){
-		$("#img7").attr("src","source/timebar2.jpg");
-	})
 
 	$("#img8").hover(function(){
 		$("#img8").attr("src","source/timebarhover3.jpg");
 	},function(){
 		$("#img8").attr("src","source/timebar3.jpg");
 	})
+
 
 	$("#img1").click(function(){
 		$("#new1").css("display","block");
